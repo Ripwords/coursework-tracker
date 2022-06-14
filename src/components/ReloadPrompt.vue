9bb29e4fdef340801f464cf590365555d7c7985f
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2022-06-14 20:43:20 +0800

    update reloadprompt

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -16,50 +16,15 @@ const close = async () => {
 </script>
 
 <template>
-  <div v-if="offlineReady || needRefresh" class="pwa-toast" role="alert">
-    <div class="message">
-      <span v-if="offlineReady">App ready to work offline</span>
-      <span v-else>New content available, click on reload button to update.</span>
+  <n-card v-if="offlineReady || needRefresh" role="alert"
+    style="width: 300px; height: auto; position: fixed; right: 15px; bottom: 15px;">
+    <div role="alert">
+      <div class="mb-3">
+        <span v-if="offlineReady">App ready to work offline</span>
+        <span v-else>New content available, click on reload button to update.</span>
+      </div>
+      <n-button size="small" v-if="needRefresh" @click="updateServiceWorker()">Reload</n-button>
+      <n-button size="small" @click="close">Close</n-button>
     </div>
-    <n-button size="small" v-if="needRefresh" @click="updateServiceWorker()">Reload</n-button>
-    <n-button size="small" @click="close">Close</n-button>
-  </div>
+  </n-card>
 </template>
-
-<style scoped>
-@media (prefers-color-scheme: dark) {
-  .pwa-toast {
-    background-color: #1e1e1e;
-    color: rgb(222, 222, 222);
-  }
-}
-@media (prefers-color-scheme: light) {
-  .pwa-toast {
-    background-color: rgb(255, 255, 255);
-    color: #1e1e1e;
-  }
-}
-.pwa-toast {
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  margin: 16px;
-  padding: 12px;
-  border: 1px solid #8885;
-  border-radius: 4px;
-  z-index: 1;
-  text-align: left;
-  box-shadow: 3px 4px 5px 0 #8885;
-}
-.pwa-toast .message {
-  margin: 8px;
-  margin-top: 4px;
-}
-.pwa-toast button {
-  border: 1px solid #8885;
-  outline: none;
-  margin-right: 5px;
-  border-radius: 2px;
-  padding: 3px 10px;
-}
-</style>
